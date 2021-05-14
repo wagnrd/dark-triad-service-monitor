2617c1f4712a849299886196a5777d69d15e6c86
Denis Wagner <denis.wagner@valtech-mobility.com>
2021-05-14 16:42:30 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Monitor from "./Monitor.svelte";
+</script>
+
+<main>
+    <Monitor name="Database API" url="some url"/>
+    <Monitor name="Login Service" url="some url"/>
+    <Monitor name="Public Database API" url="some url" diabled/>
+    <Monitor name="Game Server" url="some url" diabled/>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+    }
+</style>
---
+++ src/Monitor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let name: string;
+    export let url: string;
+    export let diabled = false;
+</script>
+
+<main>
+    <div class="caption">
+        { name }
+    </div>
+    <br/>
+    Url: { url }
+</main>
+
+<style>
+    main {
+        margin: 20px;
+        padding: 20px;
+        min-width: 300px;
+        background-color: #f4f4f4;
+        border-radius: 10px;
+    }
+
+    .caption {
+        font-size: 20px;
+    }
+</style>
